7b6092bea14fc130c93969d521e874086f74f999
FrancoUR10 <47418302+FrancoUR10@users.noreply.github.com>
2019-02-10 20:35:45 -0300

    Recuperatorio

---
+++ 2018_1Cuatri_Recu_fuentes/tres.js
@@ -0,0 +1,12 @@
+function mostrar()
+{
+    var precio
+    var descuento
+    var resultado
+    precio = prompt("Escriba el precio");
+    descuento = prompt("Escriba el descuento");
+    precio = parseInt (precio);
+    descuento = parseInt (descuento);
+    resultado = document.getElementById("elPrecioFinal").value = (precio * descuento / 100);
+    
+}
