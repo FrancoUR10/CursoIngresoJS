80afee01c68fd715e18f6cfbf1bd5fba59f9a6ff
FrancoUR10 <47418302+FrancoUR10@users.noreply.github.com>
2019-02-10 21:59:46 -0300

    Parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
-
+    var ancho
+    var largo
+    var perimetro
+    ancho = prompt("Escriba el ancho de un rectángulo");
+    largo = prompt("Escriba el largo de un rectángulo");
+    ancho = parseInt (ancho);
+    largo = parseInt (largo);
+    perimetro = (ancho * 2) + (largo * 4);
+    alert("El perimetro de un rectángulo es: "+perimetro);
 }
