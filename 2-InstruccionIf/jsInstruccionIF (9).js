dceee066929f1c4ca67b349c55818fec731ab7fe
FrancoUR10 <47418302+FrancoUR10@users.noreply.github.com>
2019-02-15 21:23:04 -0300

    InstruccionIf

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -5,10 +5,3 @@ function mostrar()
 	var random = Math.floor(Math.random()*10)+1;
 	alert("El número random es: "+random);
 }//FIN DE LA FUNCIÓN
-/*
-(min,max) (0 , 1)
-Math.floor(Math.random()*10)+1
-                          11-1
-Math.floor(Math.random()*max-min))+minimo
-
-*/
