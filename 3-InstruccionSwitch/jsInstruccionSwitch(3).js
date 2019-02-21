3903dc247f721ce0439cb3ce1378d5650f0c2a89
FrancoUR10 <47418302+FrancoUR10@users.noreply.github.com>
2019-02-21 12:28:16 -0300

    Instrucción Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,19 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
 var mesDelAño = document.getElementById('mes').value;
+switch (mesDelAño)
+{
+  case ("Febrero") :
+    alert(mesDelAño+" tiene más de 29 días.");
+    break;
+
+  default :
+    alert(mesDelAño+" tiene 30 o más días.");
+    break;
+}
+
+
 
 //alert (mesDelAño);
-	
-	
-
-
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,37 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
 var mesDelAño = document.getElementById('mes').value;
+switch (mesDelAño)
+{
+  case ("Febrero") :
+    alert(mesDelAño+" tiene 28 días.");
+    break;
+
+  case ("Enero") :
+
+  case ("Marzo") :
+
+  case ("Mayo") :
+
+  case ("Julio") :
+
+  case ("Agosto") :
+
+  case ("Octubre") :
+
+  case ("Diciembre") :
+    alert(mesDelAño+" tiene 31 días");
+    break;
+
+  default :
+    alert(mesDelAño+" tiene 30 días");
+    break;
+}
+
+
+
 
 //alert (mesDelAño);
-	
-	
-
-
 
 }//FIN DE LA FUNCIÓN
